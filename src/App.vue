1d983dba7f9af653f7643eadd17bd7cc100fd4f1
Melinda <melinda.sava.td@gmail.com>
2021-07-28 15:20:10 +0200

    added axios and fetched data to populate bar list

--- src/App.vue
+++ src/App.vue
@@ -2,50 +2,33 @@
   <div class="app">
     <h1>Hello there</h1>
     <p class="intro">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, exercitationem? Commodi beatae molestiae at. Libero corporis, cupiditate sapiente consequatur suscipit dolor quibusdam sunt mollitia expedita voluptas. Pariatur magnam illum quod.</p>
-    <Brewerys :brewerys="brewerys" />
+    <Brewerys v-if="showBars" :brewerys="brewerys" />
+    <div v-else class="loading">loading...</div>
     <Map></Map>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, ref } from 'vue'
 import Brewerys from './components/Brewerys.vue'
-import Map from './components/Map.vue';
-import { Bar } from './types/Bar';
+import Map from './components/Map.vue'
+import { Bar } from './types/Bar'
+import axios from 'axios'
 
 export default defineComponent({
   name: 'App',
   components: { Brewerys, Map },
   setup() {
-    const brewerys = ref<Bar[]>([
-        {
-        "id": 11767,
-        "obdb_id": "ironbark-brewery-jackson",
-        "name": "Ironbark Brewery",
-        "brewery_type": "micro",
-        "street": "2610 Kibby Rd",
-        "address_2": null,
-        "address_3": null,
-        "city": "Jackson",
-        "state": "Michigan",
-        "county_province": null,
-        "postal_code": "49203-4908",
-        "country": "United States",
-        "longitude": "-84.43762976",
-        "latitude": "42.2188971",
-        "phone": "5177487988",
-        "website_url": null,
-        "updated_at": "2018-08-24T00:00:00.000Z",
-        "created_at": "2018-07-24T00:00:00.000Z"
-        }
-      ])   
+    const showBars = ref(false)
+    const brewerys = ref<Bar[]>()   
       
-    onMounted( () => {
-      console.log('component mounted')
-      
-    })
+    axios.get('https://api.openbrewerydb.org/breweries')
+      .then(res => {
+        showBars.value = true
+        brewerys.value = res.data
+      })
 
-    return { brewerys }  
+    return { showBars, brewerys }  
   }
 });
 </script>
